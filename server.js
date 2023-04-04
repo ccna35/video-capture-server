a299ef4bcc0aa6569ac73f324bad9e3318d25447
ccna35 <shawkykhalil@gmail.com>
2023-04-04 13:10:20 +0200

    "First commit"

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const port = process.env.PORT || 3000;
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5500/"],
+    methods: ["GET", "POST"],
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
+});
+
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+
+// server.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
